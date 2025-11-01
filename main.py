df8b209d9678e6687fef711897a22387bcba2abd
TamaevAlbert151 <tamaevalbert151@gmail.com>
2025-11-01 17:54:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ print("Hello world!") # В этой строке выводится привет
 
 # Новая функция: печать текущей даты и времени
 current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(f"Current time is: {current_time}.") # Исправлено форматирование и добавлена точка
+print(f"Время: {current_time}.") # Исправлено форматирование и добавлена точка
