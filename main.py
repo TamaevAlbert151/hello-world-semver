c1936c6ed0adfa0b4f0b366d5c29f2da97cc7376
TamaevAlbert151 <tamaevalbert151@gmail.com>
2025-11-01 17:13:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-#main
-print("Hello world!")
+# основной файл для проекта hello-world-semver
+import datetime
+
+print("Hello world!") # В этой строке выводится приветствие
+
+# Новая функция: печать текущей даты и времени
+current_time = datetime.datetime.now()
+print(f"Current time is: {current_time}")
